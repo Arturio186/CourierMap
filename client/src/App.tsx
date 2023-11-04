2c567e7d5c768c8d8d73dffb1eecf3f6b7867513
Arturio186 <bain.romanoff@mail.ru>
2023-11-04 19:14:42 +0300

    Init React Project

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import './App.scss';
+
+
+
+const App : React.FC = () => {
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    )
+}
+
+export default App;
