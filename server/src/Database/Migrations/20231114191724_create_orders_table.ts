3c7cb1bd0e4780170c265432dc7216be295a2f37
Arturio186 <bain.romanoff@mail.ru>
2023-12-05 19:08:19 +0500

    migr

--- server/src/Database/Migrations/20231114191724_create_orders_table.ts
+++ server/src/Database/Migrations/20231114191724_create_orders_table.ts
@@ -2,20 +2,19 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex) : Promise<void> {
-    return knex.schema
-    .createTable('orders', function(table:any) {
-        table.increments('id').primary();
-        table.integer('courier_id').references('users.id');
+    return knex.schema.createTable('orders', table => {
+            table.increments('id').primary();
+            table.integer('courier_id').references('users.id');
+            
+            table.integer('status').defaultTo(1);
+            table.string('address', 200);
+            table.string('note', 200);
+            table.string('client_name', 50);
+            table.string('client_phone', 25);
         
-        table.integer('status').defaultTo(1);
-        table.string('address', 200);
-        table.string('note', 200);
-        table.string('client_name', 50);
-        table.string('client_phone', 25);
-       
-        table.timestamp('order_time');
-        table.timestamp('delivery_time');
-      });
+            table.timestamp('order_time');
+            table.timestamp('delivery_time');
+        });
 };
 
 export async function down(knex: Knex) : Promise<void> {
